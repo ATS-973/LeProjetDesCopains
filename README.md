462d2d456c2aa40d715358e9de26afa3aac84648
ATS_973 <82520816+ATS-973@users.noreply.github.com>
2023-11-20 11:23:12 +0100

    On branch backend Changes to be committed: 	modified:   LeProjetDesCopains/urls.py 	new file:   README.md 	modified:   userAccount/models.py 	new file:   userAccount/urls.py

---
+++ README.md
@@ -0,0 +1,4 @@
+A faire pour les comptes utilisateurs: 
+
+Faire en sorte que le modèle puisse rester blanc pour le CV en fonction de la méthode choisie
+Finir le form et les views
--- userAccount/models.py
+++ userAccount/models.py
@@ -2,5 +2,15 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    # Ajoutez des champs personnalisés si nécessaire
-    pass
+    name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    actualActivity = models.CharField(max_length=500)
+    email = models.EmailField(unique=True)
+    date_of_birth = models.DateField()
+    profile_picture = models.ImageField(upload_to='profile_pics/') #A modifier pour pointer vers le dossier
+    adress = models.TextField()
+    cv = models.CharField(max_length=1000)
+    genPres = models.TextField()
+    threeGoodPoints = models.TextField()
+    availability = models.ArrayField(models.CharField(max_length=100))
+    experience = models.TextField()
