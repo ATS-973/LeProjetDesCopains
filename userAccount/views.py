b6479950a703b7faf39fe3a42aaf026a10ad3567
ATS_973 <82520816+ATS-973@users.noreply.github.com>
2023-11-20 11:22:27 +0100

    On branch backend Changes to be committed:     new file:   userAccount/forms.py 	modified:   userAccount/models.py 	modified:   userAccount/views.py
    Changes not staged for commit:
    	modified:   LeProjetDesCopains/urls.py
    	modified:   userAccount/models.py
    
    Untracked files:
    	README.md
    	userAccount/urls.py

--- userAccount/views.py
+++ userAccount/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect()  # Inclure le nom de la page WEB accueil
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form}) # Modifier pour pointer vers la page de création de compte
