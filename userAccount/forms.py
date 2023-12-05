a698a72a688b3a77b76cb42c802e79536706182e
ATS_973 <82520816+ATS-973@users.noreply.github.com>
2023-12-05 07:54:43 +0100

    Ajout de la base de données utilisateurs et du formulaire de création d'un utilisateur

--- userAccount/forms.py
+++ userAccount/forms.py
@@ -5,4 +5,15 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ('username', 'email')
+        fields = ['profile_picture', 'name', 'first_name', 'actualActivity', 'email', 'phone_number', 'cv', 'genPres', 'threeGoodPoints', 'threeExperiences', 'availability']
+        labels = {'profile_picture':'Une photo de vous', 
+                  'name':'Nom', 
+                  'first_name':'Prénom', 
+                  'actualActivity':'A préciser', 
+                  'email':'Email utilisé la connexion au compte',
+                  'phone_number':'Numéro de téléphone', 
+                  'cv':'Lien vers votre CV',
+                  'genPres':'Présentation générale',
+                  'threeGoodPoints':'Trois qualités qui vous définissent',
+                  'threeExperiences':'Maximum trois expériences professionelles'}
+        required = {'cv': False if ('genPres' and 'threeGoodPoints' and 'threeExperiences') == None else True}
--- userAccount/models.py
+++ userAccount/models.py
@@ -2,15 +2,18 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
+    user_id = models.AutoField(primary_key=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/')
     name = models.CharField(max_length=100)
     first_name = models.CharField(max_length=100)
     actualActivity = models.CharField(max_length=500)
     email = models.EmailField(unique=True)
     date_of_birth = models.DateField()
-    profile_picture = models.ImageField(upload_to='profile_pics/') #A modifier pour pointer vers le dossier
-    adress = models.TextField()
-    cv = models.CharField(max_length=1000)
-    genPres = models.TextField()
-    threeGoodPoints = models.TextField()
-    availability = models.ArrayField(models.CharField(max_length=100))
-    experience = models.TextField()
+    adress = models.TextField(max_length=250)
+    cv = models.URLField(null=True)
+    genPres = models.TextField(null=True, max_length=1000)
+    threeGoodPoints = models.TextField(null=True)
+    availability = models.ArrayField(models.CharField(max_length=100)) #A modifier pour choix multiple
+    threeExperiences = models.TextField(null=True)
+    phone_number = models.CharField(max_length=20)
+    #Ajouter le tableau des horaires
