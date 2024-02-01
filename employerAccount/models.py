dc77e8a8a3ae6953731e50b7281cdfa47d6260f6
Achille THIBON-SOULA <bgdc973@lavache.com>
2024-02-01 18:23:53 +0100

    Ajout de la vérifiacation du Siret

--- employerAccount/models.py
+++ employerAccount/models.py
@@ -5,7 +5,7 @@ import api_insee
 
 class CustomEmployerUser(AbstractBaseUser, PermissionsMixin):
     employer_id = models.AutoField(primary_key=True)
-    nb_siret = models.CharField(unique=True, max_length=20)
+    nb_siret = models.CharField(unique=True, max_length=14)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
@@ -14,5 +14,22 @@ class CustomEmployerUser(AbstractBaseUser, PermissionsMixin):
     
     USERNAME_FIELD = 'nb_siret'
 
-    ### Algorithm de Luhn pour vérifier le siret
-    ### Plus surcherger save (voir chatGPT)
+    def luhnAlgorythm(self, nb_siret):
+        pos = len(nb_siret)-1
+        posPar = 1
+        somme = 0
+        for i in range(len(nb_siret)-1, 0, -1):
+            if posPar%2 == 0:
+                somme += 2*int(nb_siret[i])
+            else:
+                somme += int(nb_siret[i])
+        if somme%10 == 0:
+            return(True)
+        else:
+            return(False)
+
+    def save(self, *args, **kwargs):
+        if not self.luhnAlgorythm(self.nb_siret):
+            raise ValueError("Le numéro SIRET n'est pas valide")
+
+        super().save(*args, **kwargs)
